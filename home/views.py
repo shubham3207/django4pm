aa16b17a751d68769a4f93bd4ad06115eb177914
shubham sapkota <shubham.sapkota1@gmail.com>
2023-06-30 16:15:53 +0545

    loading brands

--- home/views.py
+++ home/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.views import View
-
+from .models import *
+class Base(View):
+    
+    views={}
 # Create your views here.
-class HomeView(View):
+class HomeView(Base):
     def get(self,request):
-        return render(request,'index.html')
+        self.views['categories']=Category.objects.all()
+        self.views['brands']=Brand.objects.all()
+        return render(request,'index.html',self.views)
+
+    
+
